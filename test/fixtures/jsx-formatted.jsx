4d28556e4c8885cfcefe8c19481a2b4ecfdce18e
Roy Riojas <roy@kno.com>
2015-02-27 01:31:11 -0800

    TST: Add test for the case of an already formatted component

---
+++ test/fixtures/jsx-formatted.jsx
@@ -0,0 +1,56 @@
+var Cards = React.createClass({
+  getInitialState: function() {
+    return {
+      allToggled: false
+    };
+  },
+  onToggled: function(cardId, state) {
+    var result = this.props.cards.filter(function(card) {
+      return cardId === card.id;
+    });
+    if (result.length > 0) {
+      result[0].toggled = state;
+    }
+    var allToggled = this.props.cards.reduce(function(prev, card) {
+      console.log('prev', prev, card.toggled);
+      return prev && card.toggled;
+    }, true);
+
+    console.log('>>>cards', this.props.cards, allToggled);
+
+    this.setState({
+      allToggled: allToggled
+    });
+  },
+  render: function() {
+    var footerClass = 'final message';
+    if (this.state.allToggled) {
+      footerClass += ' on';
+    }
+    var me = this;
+    var cards = this.props.cards.map(function(card) {
+      var pos = {
+        top: 0,
+        left: card.id * 310
+      };
+
+      return <Card card={card} key={card.id} pos={pos} onToggled={me.onToggled} />;
+    });
+    var dataProps = {
+      'data-some-val': 1
+    };
+    return <div>
+             <div className="message" {...dataProps}>
+               <p> Lo que te voy a decir no es un secreto...
+                 <br /> Pero igual deberás descubrirlo </p>
+             </div>
+             <div className="stage">
+               {cards}
+             </div>
+             <div className={footerClass}>
+               <p> Y no me cansaré de repetírtelo...
+                 <br /> Me haces muy feliz! </p>
+             </div>
+           </div>;
+  }
+});
