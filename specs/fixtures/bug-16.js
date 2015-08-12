53941aa0f6c5f91e5cee7c2f91999f2056ae40cb
royriojas <roy@kno.com>
2015-08-12 00:46:59 -0700

    FIX: properly handle ObjectExpressions and ArrayExpressions inside JSXContainers

---
+++ specs/fixtures/bug-16.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var Button = require('react-bootstrap');
+var Testing = React.createClass({
+
+    getDefaultProps: function () {
+        return {};
+    },
+
+    render: function(){
+
+        return (
+            <div {...this.props} >
+
+                    <h3 style={{  padding: "1em", textAlign: "center" }} other={[1231,1,232]} ><span >This is an empty page. To add new component select needed element on left-side panel and click on an element on the page where you want to put new component.</span></h3>
+
+                    <Button bsStyle={'primary'} ><span >Text</span></Button>
+
+
+            </div>
+        );
+    }
+});
+
+module.exports = Testing;
