b968dbf40e4b4ad258ab4b53fe3cbb8c7f1af61f
royriojas <roy@kno.com>
2015-10-06 19:39:36 -0500

    FIX: support `ExportNamedDeclaration`. Fixes #28

---
+++ specs/expected/bug-28.js
@@ -0,0 +1,7 @@
+export CounterButton1 from './CounterButton/CounterButton';
+
+const abc = 1;
+
+export { abc };
+
+export { abc as abc2 };
---
+++ specs/fixtures/bug-28.js
@@ -0,0 +1,13 @@
+export
+CounterButton1
+      from
+'./CounterButton/CounterButton';
+
+const
+abc = 1;
+
+export {
+  abc };
+
+export { abc
+  as abc2 };
