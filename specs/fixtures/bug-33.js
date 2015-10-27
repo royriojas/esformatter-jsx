9e06f360662239338c0a47c3ad07e6cd8c8c22e7
royriojas <roy@kno.com>
2015-10-26 21:21:21 -0500

    FIX: support async/await

---
+++ specs/fixtures/bug-33.js
@@ -0,0 +1,25 @@
+export    async function getByEmailAndPassword(email, password) {
+  const isValid =    await validateEmail(email);
+
+  if (!isValid) {
+     return { token: 'INVALID_EMAIL' }
+  }
+
+  const matchingUser = await usersRepo.getUserByEmail(email);
+
+  if (!matchingUser) {
+    return { token: 'EMAIL_AND_PASSWORD_MISMATCH' };
+  }
+
+  const isCorrect = await compare(password, matchingUser.get('password'));
+
+  if (!isCorrect) {
+    return { token: 'EMAIL_AND_PASSWORD_MISMATCH' };
+  }
+
+  return {
+    email: user.get('email'),
+    first_name: user.get('first_name'),
+    last_name: user.get('last_name'),
+  };
+}
