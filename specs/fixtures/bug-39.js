5e3f334d1a50f95c39f2771c3f02c639e510e526
royriojas <roy@kno.com>
2015-11-13 03:41:28 -0800

    FIX: Properly format code that contains a Bind Expression. Fixes #39

---
+++ specs/fixtures/bug-39.js
@@ -0,0 +1,21 @@
+export default class Register extends Component {
+
+  render() {
+    /*esfmt-ignore-start*/
+    const {
+      forceValidation,
+      name,
+      email,
+      password,
+      formState,
+      errorMessage,
+      validEmail
+    } = this.state;
+
+    /*esfmt-ignore-end*/
+
+    const txtClass = formSheet.t( 'l-text-entry' );
+
+    return <div onTap={ ::this.handleLogin }/>;
+  }
+}
