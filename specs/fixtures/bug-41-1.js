ed76453d4c89490d2d94093d4a6480f620089b45
Roy Riojas <royriojas@gmail.com>
2015-11-22 14:31:01 -0800

    FIX: Proper fix for #41. Properly handle the async/await in class methods

---
+++ specs/fixtures/bug-41-1.js
@@ -0,0 +1,32 @@
+import bookshelf from '../../../dal/bookshelf.js';
+import knexConfig from '../../../database/knexfile';
+import schema from '../../../database/BaseSchema.js';
+import DatabaseConnection from './databaseConnection';
+
+class DatabaseUnitOfWork {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async setUpAsync() {
+    await this.setUpMetaConnectionAsync();
+await this.connection.dropDatabaseAsync(this.config.connection.database);
+await this.connection.createDatabaseAsync(this.config.connection.database);
+await this.tearDownConnectionAsync();
+await this.setUpDataConnectionAsync();
+await this.connection.executeMigrationsAsync();
+await this.tearDownConnectionAsync();
+await bookshelf.factory.knex.client.initializePool(knexConfig);
+  }
+
+  async tearDownAsync() {
+    await bookshelf.factory.knex.destroy();
+  await this.setUpMetaConnectionAsync();
+  await this.connection.dropDatabaseAsync(this.config.connection.database);
+  await this.tearDownConnectionAsync();
+  }
+}
+
+export function createUnitOfWork() {
+  return new DatabaseUnitOfWork(knexConfig);
+}
