a8940a02ab583dc4593f8fa2af69d3f312002ee7
royriojas <royriojas@gmail.com>
2015-11-30 12:55:06 -0500

    FIX: support decorators in classMethods. Fixes #43

---
+++ specs/expected/bug-43.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { injectProps, decorator1, decorator2 } from '../../helpers/injectProps.js';
+
+@injectProps
+export default class PlusActionButton extends Component {
+
+  @injectProps
+  @decorator1
+  @decorator2
+  render({style, children}) {
+    return (
+      <div
+           className="fixed-action-btn"
+           style={ style }>
+        <a className="btn-floating btn-large blue"><i className="large material-icons">add</i></a>
+        <ul>
+          { children }
+        </ul>
+      </div>
+      );
+  }
+}
