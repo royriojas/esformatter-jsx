ac2dc2d718821fb9cb89dedc06eea006fa47d3b4
royriojas <royriojas@gmail.com>
2015-12-01 14:39:24 -0500

    FIX: properly format code with comments in decorators. Fix #45

---
+++ specs/fixtures/bug-45.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import injectProps from '../../helpers/injectProps';
+
+
+export default class Form extends Component {
+
+  @injectProps // esfmt-ignore-line
+  render({memberTypes}) {
+
+    return (
+    <div style={ {  minWidth: '400px'} }>
+      <h4>New Title</h4>
+      { /* Modal Form With Fields */ }
+    </div>
+    );
+  }
+}
