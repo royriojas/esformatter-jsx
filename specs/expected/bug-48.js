6ce091b00ad224bd40d67f95c78b6a2a239aaf73
royriojas <royriojas@gmail.com>
2015-12-25 07:12:41 -0500

    FEAT: Add option to avoid formatting of JSXExpressions. Fixes #48
    This commit adds a new option `formatJSXExpressions`. The default value is true.
    
    In case you need to stop this behavior, for example when using a plugin that modify the code like the `esformatter-semicolons`.
    
    You should opt out of the formatting of the JSXExpressions to avoid potentially risky issues
    
    ```javascript
    // example of the options
    {
      "jsx": {
        "formatJSX": true,
        "attrsOnSameLineAsTag": true,
        "maxAttrsOnTag": 3,
        "firstAttributeOnSameLine": true,
        "spaceInJSXExpressionContainers": " ",
        "alignWithFirstAttribute": false,
        // default is true, setting it to false JSXExpressions won't be recursively formatted
        "formatJSXExpressions": false,
        "htmlOptions": {
          "brace_style": "collapse",
          "indent_char": " ",
          "indent_size": 2,
          "max_preserve_newlines": 2,
          "preserve_newlines": true
        }
      }
    }
    ```

---
+++ specs/expected/bug-48.js
@@ -0,0 +1,15 @@
+export default (props) => <div className='ui item'>
+                            <div className="ui middle aligned grid">
+                              <div className="three wide column">
+                                <img className="ui large circular image" src={ (abc && cd)     ? 4 : 1 } />
+                              </div>
+                              <div className="thirteen wide column">
+                                <div className='ui tiny header'>
+                                  { props.header }
+                                  <div className='ui sub header'>
+                                    { props.subheader }
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
