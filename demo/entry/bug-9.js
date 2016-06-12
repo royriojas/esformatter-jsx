d8c4bf88dc3849be8de0bf9f62149132da0b91e8
Roy Riojas <royriojas@gmail.com>
2016-06-12 04:33:20 -0500

    FIX: closes #76. don't replace jsxElements on `stringBefore` hook

--- demo/entry/bug-9.js
+++ demo/entry/bug-9.js
@@ -1,16 +1,5 @@
-@decorator({
-  some: 'value',
-})
-export default class Example {
-  var1;
-  @readonly
-  variable=2;
-
-  @readonly(true)
-  variable3   = 3;
-
-  @someDecotator var2;
-static v;
-static v = { a, b, c};
-
+function render(count) {
+  return <input type="text"
+            onChange={this.change("some nice")}
+            value={word} />;
 }
--- demo/runner.js
+++ demo/runner.js
@@ -1,5 +1,6 @@
 var esformatter = require( 'esformatter' );
 esformatter.register( require( '../lib/plugin' ) );
+esformatter.register( require('esformatter-quotes') );
 var chalk = require( 'chalk' );
 var fs = require( 'fs' );
 var mkdirp = require( 'mkdirp' );
@@ -12,10 +13,24 @@ files.forEach( function ( file ) {
   var codeStr = fs.readFileSync( './entry/' + file ).toString();
   var options = {
     "jsx": {
-      "attrsOnSameLineAsTag": false,
-      "maxAttrsOnTag": 1,
-      "firstAttributeOnSameLine": false,
-      "alignWithFirstAttribute": false
+      "formatJSX": true,
+      "attrsOnSameLineAsTag": true,
+      "maxAttrsOnTag": 3,
+      "firstAttributeOnSameLine": true,
+      "spaceInJSXExpressionContainers": " ",
+      "alignWithFirstAttribute": false,
+      "formatJSXExpressions": true,
+      "htmlOptions": {
+        "brace_style": "collapse",
+        "indent_char": " ",
+        "indent_size": 2,
+        "max_preserve_newlines": 2,
+        "preserve_newlines": true
+      }
+    },
+    "quotes": {
+      "type": "single",
+      "avoidEscape": true
     }
   };
 
