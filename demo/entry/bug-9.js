5b60df1ca64e185707102616f110240a11e0ff03
Roy Riojas <royriojas@gmail.com>
2017-04-12 17:20:30 -0500

    BLD: Upgrade demo test

--- demo/entry/bug-9.js
+++ demo/entry/bug-9.js
@@ -1,5 +1,16 @@
-const renderFn = () => {
-  return <div className="some" data-attribute='an attribute" value'>
-    <div>Hello world</div>
-  </div>;
-};
+// Actual result
+import { Row, Col } from 'react-bootstrap';
+
+const Status = props => (
+  <Row>
+<Col md={ 5 }><strong>Status</strong>
+<a>
+  <p>
+    <div> <strong>{ props.test }</strong> </div>
+  </p>
+</a>
+</Col>
+<Col md={ 2 }><strong>Door</strong></Col>
+<Col md={ 5 }><strong>Om</strong></Col>
+  </Row>
+);
